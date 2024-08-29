a6360b4ba90a57e74947e0ea9d072f7a923bfe03
jalho <j.pd.alho@gmail.com>
2024-08-29 11:59:27 +0100

    [neovim] add nvim-metals LSP

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -193,4 +193,40 @@ return {
 			})
 		end,
 	},
+	{ -- Scala LSP using metals
+		"scalameta/nvim-metals",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"j-hui/fidget.nvim",
+				opts = {},
+			},
+		},
+		ft = { "scala", "sbt" },
+		opts = function()
+			local metals_config = require("metals").bare_config()
+
+			-- Example of settings
+			metals_config.settings = {
+				showImplicitArguments = true,
+				excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+			}
+
+			metals_config.init_options.statusBarProvider = "off"
+
+			metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			return metals_config
+		end,
+		config = function(self, metals_config)
+			local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = self.ft,
+				callback = function()
+					require("metals").initialize_or_attach(metals_config)
+				end,
+				group = nvim_metals_group,
+			})
+		end,
+	},
 }
