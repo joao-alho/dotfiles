ccceab95ca58aea5e5fcc22a9c834004f3b75700
jalho <j.pd.alho@gmail.com>
2024-10-05 16:44:13 +0100

    [neovim] colorscheme changes for transparency options

--- .config/nvim/lua/custom/plugins/colorschemes.lua
+++ .config/nvim/lua/custom/plugins/colorschemes.lua
@@ -6,9 +6,16 @@ return {
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
+			local scheme = require("rose-pine")
+			scheme.setup({
+				styles = {
+					transparency = false,
+				},
+				dim_inactive_windows = true,
+			})
 			vim.cmd.colorscheme("rose-pine")
 			-- You can configure highlights by doing something like
-			vim.cmd.hi("Comment gui=none")
+			-- vim.cmd.hi("Comment gui=none")
 		end,
 	},
 }
