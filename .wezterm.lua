28a4db5b5a6e6fecef1e336e646815529b5bff21
jalho <j.pd.alho@gmail.com>
2024-10-05 16:44:48 +0100

    [wezterm] initial wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "rose-pine"
+config.window_background_opacity = 0.9
+config.font = wezterm.font({ family = "JetBrains Mono", weight = "Regular" })
+config.font_size = 12
+
+config.enable_wayland = true
+config.window_decorations = "NONE"
+config.hide_tab_bar_if_only_one_tab = false
+config.show_new_tab_button_in_tab_bar = false
+config.window_frame = {
+	font = wezterm.font({ family = "JetBrains Mono", weight = "Bold" }),
+	font_size = 10,
+}
+
+wezterm.on("update-status", function(window)
+	-- Grab the utf8 character for the "powerline" left facing
+	-- solid arrow.
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Grab the current window's configuration, and from it the
+	-- palette (this is the combination of your chosen colour scheme
+	-- including any overrides).
+	local color_scheme = window:effective_config().resolved_palette
+	local bg = color_scheme.background
+	local fg = color_scheme.foreground
+
+	window:set_right_status(wezterm.format({
+		-- First, we draw the arrow...
+		{ Background = { Color = "none" } },
+		{ Foreground = { Color = bg } },
+		{ Text = SOLID_LEFT_ARROW },
+		-- Then we draw our text
+		{ Background = { Color = bg } },
+		{ Foreground = { Color = fg } },
+		{ Text = " " .. wezterm.hostname() .. " " .. wezterm.strftime("%a %b %-d %H:%M") .. " " },
+	}))
+end)
+
+return config
