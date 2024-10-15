548535dc2e8d10196c661d95b967346ce6bcfd96
jalho <j.pd.alho@gmail.com>
2024-10-15 09:28:48 +0100

    [neovim] comment out buffer cmp

--- .config/nvim/lua/custom/plugins/completions.lua
+++ .config/nvim/lua/custom/plugins/completions.lua
@@ -19,7 +19,7 @@ return {
 			"saadparwaiz1/cmp_luasnip",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-buffer",
+			-- "hrsh7th/cmp-buffer",
 			{
 				-- "MattiasMTS/cmp-dbee",
 				dir = "~/projects/nvim/cmp-dbee/",
