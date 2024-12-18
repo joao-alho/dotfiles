624e1678b65a31c44acf0df8797507d386a8c598
jalho <j.pd.alho@gmail.com>
2024-12-18 12:27:15 +0000

    [neovim] update iron

--- .config/nvim/lua/custom/plugins/iron.lua
+++ .config/nvim/lua/custom/plugins/iron.lua
@@ -1,22 +1,24 @@
 return {
-	"Vigemus/iron.nvim",
-	keys = { { "<leader>if", "<cmd>IronFocus scala<cr>", desc = "Iron" } },
-	opts = {},
-	config = function()
-		local iron = require("iron.core")
+	{
+		"Vigemus/iron.nvim",
+		opts = {},
+		config = function()
+			local iron = require("iron.core")
 
-		iron.setup({
-			config = {
-				scratch_repl = true,
-				repl_definition = {
-					scala = {
-						command = { "docker", "exec", "-it", "devices", "./iceberg-shell.sh" },
+			iron.setup({
+				config = {
+					scratch_repl = true,
+					repl_definition = {
+						scala = {
+							command = { "docker", "exec", "-it", "devices", "./iceberg-shell.sh" },
+						},
 					},
+					ignore_blank_lines = true,
+					repl_open_cmd = require("iron.view").bottom(10),
 				},
 				ignore_blank_lines = true,
-				repl_open_cmd = require("iron.view").bottom(10),
-			},
-			ignore_blank_lines = true,
-		})
-	end,
+			})
+			require("custom.iron")
+		end,
+	},
 }
