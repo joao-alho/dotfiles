9946242f5f020d9fd00003fcfdba79e51dee1d70
jalho <j.pd.alho@gmail.com>
2025-01-20 09:47:45 +0000

    [wezterm] add config for WSL

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -184,7 +184,6 @@ return {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format lua code
-				"sql-formatter",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
--- .wezterm.lua
+++ .wezterm.lua
@@ -15,6 +15,34 @@ config.window_frame = {
 	font_size = 10,
 }
 
+config.wsl_domains = {
+	{
+		-- The name of this specific domain.  Must be unique amonst all types
+		-- of domain in the configuration file.
+		name = "WSL:Ubuntu",
+
+		-- The name of the distribution.  This identifies the WSL distribution.
+		-- It must match a valid distribution from your `wsl -l -v` output in
+		-- order for the domain to be useful.
+		distribution = "Ubuntu",
+
+		-- The username to use when spawning commands in the distribution.
+		-- If omitted, the default user for that distribution will be used.
+
+		-- username = "hunter",
+
+		-- The current working directory to use when spawning commands, if
+		-- the SpawnCommand doesn't otherwise specify the directory.
+
+		default_cwd = "~",
+	},
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_domain = "WSL:Ubuntu"
+	config.window_decorations = "RESIZE"
+end
+
 wezterm.on("update-status", function(window)
 	-- Grab the utf8 character for the "powerline" left facing
 	-- solid arrow.
