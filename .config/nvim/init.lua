a3131b1d11e7732a183dae6400bef2363d79406e
jalho <j.pd.alho@gmail.com>
2025-04-15 18:17:34 +0100

    [neovim] add default single window border

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,6 +46,7 @@ vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
+vim.opt.winborder = "single"
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
