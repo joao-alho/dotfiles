e61b78215b99c65f17c4f591253854ece09d5821
jalho <j.pd.alho@gmail.com>
2025-04-15 12:20:51 +0100

    [neovim] minor changes

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -7,8 +7,6 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			"saghen/blink.cmp",
-			-- Useful status updates for LSP.
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{
 				"j-hui/fidget.nvim",
 				opts = { notification = { window = { winblend = 0 } } },
