a7f37242d7ea7c95615a54bf17bfa619c5679129
jalho <j.pd.alho@gmail.com>
2025-05-19 07:59:29 +0100

    [neovim] update minifiles

--- .config/nvim/lua/custom/mini.lua
+++ .config/nvim/lua/custom/mini.lua
@@ -4,7 +4,17 @@ local minifiles_toggle = function()
 	end
 end
 
+local minifiles_toggle_cwf = function()
+	if not MiniFiles.close() then
+		MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+		vim.schedule(function()
+			MiniFiles.reveal_cwd()
+		end)
+	end
+end
+
 vim.keymap.set("n", "-", minifiles_toggle, { desc = "Open mini.files" })
+vim.keymap.set("n", "-f", minifiles_toggle_cwf, { desc = "Open mini.files in current working file" })
 
 local show_dotfiles = true
 
